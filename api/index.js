cea0935270981c87c48df1a2cc20b39041573226
Jasser Amri <jam@quicktext.im>
2025-10-24 18:09:22 +0100

    Fix serverless function deployment

--- api/index.js
+++ api/index.js
@@ -1,9 +1,5 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-
-// Import the main server logic
-require('../server')(app);
+// Import the Express app from server.js
+const app = require('../server');
 
 // Export the Express app as a serverless function
 module.exports = app;
